1a7f885571637fcade753b69128b84002d28098e
a369155609 <1985055390@qq.com>
2024-09-10 12:09:19 +0800

    更新一下java版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = false
@@ -55,6 +55,10 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+
+
+
+//    implementation("com.github.a369155609:ScrollText:v0.0.2")
 
     implementation(project(":scrolltext"))
 
